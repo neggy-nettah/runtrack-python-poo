9cd6be42f7a2b67e03e3dc801c5ed8cb1251f0ed
neggy-nettah <neggy.nettah@laplateforme.io>
2023-03-27 11:13:27 +0200

    job02

---
+++ job02.py
@@ -0,0 +1,11 @@
+class Operation:
+    def __init__(self):
+        self.nombre1 = 10
+        self.nombre2 = 20
+
+operation1 = Operation()
+print(f"Le nombre 1 est {operation1.nombre1}")
+print(f"Le nombre 2 est {operation1.nombre2}")
+
+
+
