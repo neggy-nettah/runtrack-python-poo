703a2486f84266efc6154c2297cc98fc355d1d71
neggy-nettah <neggy.nettah@laplateforme.io>
2023-03-27 11:19:55 +0200

    job03

---
+++ job03.py
@@ -0,0 +1,12 @@
+class Operation:
+    def __init__(self):
+        self.nombre1 = 10
+        self.nombre2 = 20
+    
+    def addition(self):
+        print(f"Resultat: {self.nombre1 + self.nombre2}")
+
+operation1 = Operation()
+operation1.addition()
+
+
